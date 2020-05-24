6b2f1187451025b4db0afa335a0558f7d47931ac
Aman-tech107 <karan.mukesh1985@gmail.com>
2020-05-24 22:30:48 +0530

    Falling Raindrops

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -28,16 +28,44 @@ raindrop = new Raindrop(5,5,5,50);
 
 function draw() {
   background(255,255,255);  
-  
+
   Engine.update(engine);
+  //for(var b = 1; b < 3; b--){
+
+    for(var a = 1; a<2000;a++){
+      var r = Math.round(random(0,99));
+      var g = Math.round(random(0,99));
+      var b = Math.round(random(0,99));
+      fill("#"+r+g+b);
+
+      array["rain"+a].display();
+      //array["rain"+a].yCordinate = 5;
+    }
+  //}
+  
   for(var a = 1; a<2000;a++){
+    if(array["rain"+a].body.position.y > 400){
+      delete array["rain"+a];
+      var xCordinate = random(0,400);
+      var yCordinate = random(0,-600);
     
-    var r = Math.round(random(0,99));
-    var g = Math.round(random(0,99));
-    var b = Math.round(random(0,99));
-    fill("#"+r+g+b);
-    array["rain"+a].display();
+      var heightrandom = random(5,10);
+      array["rain"+a] = new Raindrop(xCordinate,yCordinate,5,heightrandom);
+    }
   }
-  raindrop.display();
+
+  /*raindrop.display();
+
+  if(raindrop.body.position.y > 400){
+    raindrop = new Raindrop(5,5,200,10);
+    console.log("More than 400");
+  }*/
+  //console.log("Y Position "+raindrop.body.position.y);
+  
+
+//raindrop.display();
+
+
+
   drawSprites();
 }
